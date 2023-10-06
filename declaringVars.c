fe3b876a2ba323bc26c7f5432fe41ef4d3b14c46
Wes McKnight <wesleymcknight17@gmail.com>
2023-10-06 06:15:29 -0500

    more variables

--- declaringVars.c
+++ declaringVars.c
@@ -3,3 +3,11 @@ int myNum;
 
 // Assign a value to the variable
 myNum = 15;
+
+printf("%i", myNum);
+
+float myFloatNum = 5.99;
+char myLetter = 'D';
+
+printf("%f", myFloatNum);
+printf("%c", myLetter);
